4a0f6eb2d708bb296608e6098bee8e8cda95713f
nerdy-guy <115644166+nerdy-guy@users.noreply.github.com>
2023-05-12 04:48:53 +0300

    feat: add theme switcher

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,58 @@
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import Register from "./pages/Register";
 import Login from "./pages/Login";
+import Home from "./pages/Home";
+import { useContext } from "react";
+import { ThemeContext } from "./contexts/ThemeContext";
+import Book from "./pages/Book";
+import Layout from "./components/Layout";
 
 const App = () => {
   const router = createBrowserRouter([
     {
-      path: "/register",
-      element: <Register />,
+      path: "/",
+      element: <Layout />,
+      children: [
+        {
+          path: "/",
+          element: <Home />,
+        },
+        {
+          path: "/register",
+          element: <Register />,
+        },
+        {
+          path: "/login",
+          element: <Login />,
+        },
+        {
+          path: "/:page_id",
+          element: <Book />,
+        },
+      ],
     },
-    {
-      path: "/login",
-      element: <Login />,
-    },
+    // {
+    //   path: "/",
+    //   element: <Home />,
+    // },
+    // {
+    //   path: "/register",
+    //   element: <Register />,
+    // },
+    // {
+    //   path: "/login",
+    //   element: <Login />,
+    // },
+    // {
+    //   path: "/:page_id",
+    //   element: <Book />,
+    // },
   ]);
 
+  const { theme } = useContext(ThemeContext);
+
   return (
-    <div>
+    <div className={theme === "dark" ? "dark" : "light"}>
       <RouterProvider router={router} />
     </div>
   );
